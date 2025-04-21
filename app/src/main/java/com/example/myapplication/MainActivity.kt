42e12ac5ddb22a54c8904d81df2fa8625fcb8e06
Luke <lt.engineer98@gmail.com>
2025-04-21 00:41:58 -0400

    Added Camera funcitionality and preview

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,36 +1,156 @@
+@file:kotlin.OptIn(ExperimentalPermissionsApi::class)
+
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.OptIn
+import androidx.camera.compose.CameraXViewfinder
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraSelector.DEFAULT_FRONT_CAMERA
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.core.SurfaceRequest
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.lifecycle.awaitInstance
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import androidx.compose.runtime.remember
+
+
+class CameraPreviewViewModel : ViewModel() {
+    // Used to set up a link between the Camera and your UI.
+    private val _surfaceRequest = MutableStateFlow<SurfaceRequest?>(null)
+    val surfaceRequest: StateFlow<SurfaceRequest?> = _surfaceRequest
+
+    private val cameraPreviewUseCase = Preview.Builder().build().apply {
+        setSurfaceProvider { newSurfaceRequest ->
+            _surfaceRequest.update { newSurfaceRequest }
+        }
+    }
+
+    suspend fun bindToCamera(appContext: Context, lifecycleOwner: LifecycleOwner) {
+        val processCameraProvider = ProcessCameraProvider.awaitInstance(appContext)
+        processCameraProvider.bindToLifecycle(
+            lifecycleOwner, DEFAULT_FRONT_CAMERA, cameraPreviewUseCase
+        )
+
+        // Cancellation signals we're done with the camera
+        try { awaitCancellation() } finally { processCameraProvider.unbindAll() }
+    }
+}
+
+@Composable
+fun CameraPreviewScreen(viewModel: CameraPreviewViewModel, modifier: Modifier = Modifier) {
+    val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+    if (cameraPermissionState.status.isGranted) {
+        CameraPreviewContent(viewModel, modifier)
+    } else {
+        Column(
+            modifier = modifier.fillMaxSize().wrapContentSize().widthIn(max = 480.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+                // If the user has denied the permission but the rationale can be shown,
+                // then gently explain why the app requires this permission
+                "Whoops! Looks like we need your camera to work our magic!" +
+                        "Don't worry, we just wanna see your pretty face (and maybe some cats).  " +
+                        "Grant us permission and let's get this party started!"
+            } else {
+                // If it's the first time the user lands on this feature, or the user
+                // doesn't want to be asked again for this permission, explain that the
+                // permission is required
+                "Hi there! We need your camera to work our magic! ✨\n" +
+                        "Grant us permission and let's get this party started! \uD83C\uDF89"
+            }
+            Text(textToShow, textAlign = TextAlign.Center)
+            Spacer(Modifier.height(16.dp))
+            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                Text("Unleash the Camera!")
+            }
+        }
+    }
+}
+
 
 class MainActivity : ComponentActivity() {
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val _surfaceRequest = MutableStateFlow<SurfaceRequest?>(null)
+            val surfaceRequest: StateFlow<SurfaceRequest?> = _surfaceRequest
+            val cameraPreviewUseCase = Preview.Builder().build().apply {
+                setSurfaceProvider { newSurfaceRequest ->
+                    _surfaceRequest.update { newSurfaceRequest }
+                }
+            }
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also { it.setAnalyzer(ContextCompat.getMainExecutor(this), BarcodeAnalyzer()) }
+
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                this, CameraSelector.DEFAULT_BACK_CAMERA, cameraPreviewUseCase, imageAnalysis
+            )
+        }, ContextCompat.getMainExecutor(this))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             val borderWidth = 6;
             MyApplicationTheme {
+                val viewModel = remember { CameraPreviewViewModel() }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(modifier = Modifier.fillMaxSize().padding(innerPadding).border(borderWidth.dp, Color.Black, RectangleShape)) {
                         Row(modifier = Modifier.fillMaxSize().padding(borderWidth.dp)) {
-                            Text("Hello World")
+                            CameraPreviewScreen(viewModel, Modifier)
                         }
                     }
 
@@ -51,13 +171,40 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
+class BarcodeAnalyzer : ImageAnalysis.Analyzer {
+    private val scanner = BarcodeScanning.getClient()
 
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Lucas")
-        print(" hello")
+    @OptIn(ExperimentalGetImage::class)
+    override fun analyze(image: ImageProxy) {
+        val mediaImage = image.image
+        if (mediaImage != null) {
+            val inputImage = InputImage.fromMediaImage(mediaImage, image.imageInfo.rotationDegrees)
+            scanner.process(inputImage)
+                .addOnSuccessListener { barcodes ->
+                    barcodes.firstOrNull()?.rawValue?.let { scanResult ->
+                        // Handle result (e.g., debounce and save to DB)
+                    }
+                }
+                .addOnCompleteListener { image.close() }
+        }
     }
 }
+@Composable
+fun CameraPreviewContent(
+    viewModel: CameraPreviewViewModel,
+    modifier: Modifier = Modifier,
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+) {
+    val surfaceRequest by viewModel.surfaceRequest.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    LaunchedEffect(lifecycleOwner) {
+        viewModel.bindToCamera(context.applicationContext, lifecycleOwner)
+    }
 
+    surfaceRequest?.let { request ->
+        CameraXViewfinder(
+            surfaceRequest = request,
+            modifier = modifier
+        )
+    }
+}
